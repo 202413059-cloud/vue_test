2029296f17c7cadc18037150cf8e6adf6a4d9c3d
nueui <202413059@jbnu.ac.kr>
2025-11-07 23:33:58 +0900

    Initial Vue 3 setup and dependency update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': '/src'
+        }
+    },
+    server: {
+        port: 5173, // 기본 개발 서버 포트 (원하면 변경 가능)
+        open: true  // 실행 시 자동으로 브라우저 열기
+    },
+    build: {
+        outDir: 'dist',
+        sourcemap: false
+    }
 })
